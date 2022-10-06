a45bd4c7eeb1fa947ab6d17f5f73b8ca2d99691d
Qiuyaxian <1776296049@qq.com>
2022-10-06 13:27:28 +0800

    ci(config): 更改 cz-config.js 配置，减少流程

--- .cz-config.js
+++ .cz-config.js
@@ -29,8 +29,8 @@ module.exports = {
     // footer: '请输入要关闭的issue，例如：#12, #34(可选):\n',
     confirmCommit: '确定提交此说明吗？'
   },
-  allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'],
+  // allowCustomScopes: true,
+  // allowBreakingChanges: ['feat', 'fix'],
   // 限制 subject 长度
   subjectLimit: 100
   // 跳过问题 skip any questions you want
